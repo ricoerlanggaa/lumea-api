12fa768c6601d4591272ef8d0203ebb7afe033dc
Rico Erlangga <ricoerlanggaa@gmail.com>
2025-06-30 15:30:42 +0700

    add rate-limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +8,7 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({ throttlers: [{ ttl: 60000, limit: 10 }] }),
     MongooseModule.forRoot(process.env.MONGO_URI ?? 'mongodb://localhost:27017/lumea'),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(compression());
+  app.use(helmet());
+  app.enableCors({ origin: '*' });
+  app.enableVersioning({ defaultVersion: '1', type: VersioningType.URI });
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 5000);
 }
 void bootstrap();
